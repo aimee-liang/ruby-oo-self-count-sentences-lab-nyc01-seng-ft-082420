5d6b0eaabf68e18a4e180df260092277aae08d3e
aimee-liang <aimeeeliang@gmail.com>
2020-08-29 19:23:27 -0400

    First commit

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,24 @@
-require 'pry'
+# require 'pry'
 
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true
+    else
+      return false
+    end
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/\.|\?|\!/).delete_if{|word| word.size < 2}.size
   end
 end
